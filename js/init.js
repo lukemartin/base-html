3be95c43f0287e61484f500cd8e0541583f35978
Luke Martin <luke@lukemartin>
2012-06-13 11:58:33 +0100

    Initial base-html build

---
+++ js/init.js
@@ -0,0 +1,18 @@
+var L = (function(my, $) {
+    my.init = function() {
+    }
+
+    return my;
+}(L || {}, jQuery));
+
+
+L.Dragons = (function($) {
+    var my = {};
+
+    return my;
+}(jQuery));
+
+
+(function($) {
+    L.init();
+}(jQuery));
